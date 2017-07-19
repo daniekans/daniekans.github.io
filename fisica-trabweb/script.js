133b191475ed6fae29abc20290c1a5def498bc94
daniekans <dandanielfranco@yahoo.com.br>
2017-07-19 13:54:37 -0300

    Alterações 7
    Correções do Trabalho

--- fisica-trabweb/script.js
+++ fisica-trabweb/script.js
@@ -1,45 +1,70 @@
 // Cálculo do peso em outros planetas
 
-function getGravidade(val) {
-  let g = null;
-
-  switch (val) {
-    case 'mercurio':
-    case 'marte': g = 3.7; break;
-    case 'venus': g = 8.9; break;
-    case 'terra': g = 9.8; break;
-    case 'jupiter': g = 24.7; break;
-    case 'saturno': g = 10.4; break;
-    case 'urano': g = 8.7; break;
-    case 'netuno': g = 11.15; break;
-    case 'plutao': g = 0.6; break;
-    case 'lua': g = 1.62; break;
-    case 'sol': g = 274;
-  }
-
-  return g;
-}
-
+const gravidades = {
+  mercurio: 3.7,
+  marte: 3.7,
+  venus: 8.9,
+  terra: 9.8,
+  jupiter: 24.7,
+  saturno: 10.4,
+  urano: 8.7,
+  netuno: 11.15,
+  plutao: 0.6,
+  lua: 1.62,
+  sol: 274
+};
 
 // Indicar mudança da gravidade
 
 let botaoEl = document.querySelector('#calc-peso');
 let selectEl = document.querySelector('select#planetas');
 
-botaoEl.addEventListener('click', function() {
-  let massaEl = document.querySelector('#massa');
-  let pesoEl = document.querySelector('#peso');
+if(botaoEl && selectEl) {
 
-  if(massaEl.value >= 0) {
-    peso.style.color = 'initial';
-    peso.value = ((massaEl.value * getGravidade(selectEl.value)).toFixed(2) + ' N');
-  }
-  else {
-    peso.style.color = 'darkred';
-    peso.value = 'Massa Negativa';
-  }
-})
+  botaoEl.addEventListener('click', function() {
+    let massaEl = document.querySelector('#massa');
+    let pesoEl = document.querySelector('#peso');
 
-selectEl.addEventListener('change', function() {
-  document.querySelector('#div-planetas .mostra-g').innerHTML = ('A gravidade no corpo celeste selecionado é de ' + getGravidade(this.value) + ' m/s²');
-});
+    if(massaEl.value >= 0) {
+      peso.style.color = 'initial';
+      peso.value = ((massaEl.value * gravidades[selectEl.value]).toFixed(2) + ' N');
+    }
+    else {
+      peso.style.color = 'darkred';
+      peso.value = 'Massa Negativa';
+    }
+  })
+
+  selectEl.addEventListener('change', function() {
+    document.querySelector('#div-planetas .mostra-g').innerHTML = ('A gravidade no corpo celeste selecionado é de ' + gravidades[this.value] + ' m/s²');
+  });
+
+}
+
+// Teste...
+
+if((location.pathname.indexOf('exp') !== -1) && (location.pathname.length < 12)) {
+  let footerEl = document.querySelector('footer');
+  let btAntEl = document.createElement('BUTTON');
+  let btProxEl = document.createElement('BUTTON');
+
+  btAntEl.innerHTML = 'Experiência Anterior';
+  btProxEl.innerHTML = 'Próxima Experiência';
+  footerEl.appendChild(btAntEl);
+  footerEl.appendChild(btProxEl);
+
+  let x = parseInt(location.pathname.replace('/exp', ''));
+  if(x !== 16)
+    btProxEl.addEventListener('click', function() {
+    location.pathname = '/exp' + String(++x) + '.html';
+  });
+  else
+    btProxEl.style.backgroundColor = 'darkred';
+  if(x !== 1)
+    btAntEl.addEventListener('click', function() {
+      location.pathname = '/exp' + String(--x) + '.html';
+    });
+  else
+    btAntEl.style.backgroundColor = 'darkred';
+
+}
